0e251d663045282dd27d3c154cec757ae032f7c2
seard <sebastian_ardesjo@hotmail.com>
2022-11-13 03:05:42 +0100

    Bumped version number to 0.2.1

--- duktig/server.js
+++ duktig/server.js
@@ -101,7 +101,7 @@ const handleCommand = (json) => {
     LedController.alternate({ r: 255, g: 0, b: 155 }, { r: 0, g: 255, b: 170 }, 1000);
 
     const introductions = [
-        `Hows it hanging my neighbors. I am running Duktig version 0.2.`,
+        `Hows it hanging my neighbors. I am running Duktig version 0.2.1`,
         `My IP is ${localIpAddress()}`
     ];
 
