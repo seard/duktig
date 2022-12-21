b4a8823e09e88f7f41611d744d5a3ce99248f8cc
seard <sebastian_ardesjo@hotmail.com>
2022-12-21 16:03:20 +0100

    test

--- install/install.sh
+++ install/install.sh
@@ -5,10 +5,11 @@ set -o xtrace
 sudo rm /etc/network/if-up.d/duktig-startup.sh || true
 sudo rm /home/seard/duktig-startup.sh || true
 
-sudo cp duktig-startup.sh /etc/network/if-up.d/
-# sudo cp duktig-startup.sh /home/seard/
+# sudo cp duktig-startup.sh /etc/network/if-up.d/
+sudo cp duktig-startup.sh /home/seard/
 
 echo "Run:"
 echo "$ crontab -e"
 echo "Append:"
 echo "@reboot sleep 30 && /home/seard/duktig-startup.sh"
+#@reboot echo "Starting duktig..." && sleep 30 && /home/seard/duktig-startup.sh > /home/seard/duktig-log.txt
