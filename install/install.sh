67fe6a4a9d341171c527ded84f165a9b1b32dd97
seard <sebastian_ardesjo@hotmail.com>
2022-12-21 14:16:13 +0100

    test

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 set -o xtrace
-sudo cp duktig-startup.sh /etc/network/if-up.d/
+
+sudo rm /etc/network/if-up.d/duktig-startup.sh || true
+sudo rm /home/seard/duktig-startup.sh || true
+
+# sudo cp duktig-startup.sh /etc/network/if-up.d/
 sudo cp duktig-startup.sh /home/seard/
 
 echo "Run:"
